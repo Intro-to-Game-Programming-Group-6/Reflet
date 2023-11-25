59a8f2a305b488eec15ec11a29213791e2ae5df2
unknown <a.ariyanuchitkul.com>
2023-11-25 23:38:03 +0800

    vial update, bullet knockback system

--- Assets/Scripts/Enemies/Song_Edition/TestEnemyScript.cs
+++ Assets/Scripts/Enemies/Song_Edition/TestEnemyScript.cs
@@ -62,7 +62,7 @@ public class TestEnemyScript : MonoBehaviour
     {
         while (true)
         {
-            GameObject bullet = Instantiate(bulletPrefab, agent.transform.position, Quaternion.identity, gameObject.transform);
+            GameObject bullet = Instantiate(bulletPrefab, agent.transform.position, Quaternion.identity);
             bullet.GetComponent<TestBulletScript>().ShootAt(player);
             yield return new WaitForSeconds(attackDelay);
         }

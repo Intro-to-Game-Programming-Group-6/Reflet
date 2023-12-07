317d99c98b56cdf22b2574d30a32f93684a7ba14
Keith Saudjana <knsaudjana@gmail.com>
2023-12-07 22:01:59 +0800

    Commit 1

--- Assets/Scripts/Overworld/TutorialManager.cs
+++ Assets/Scripts/Overworld/TutorialManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
 public class TutorialManager : MonoBehaviour
 {
@@ -9,7 +10,13 @@ public class TutorialManager : MonoBehaviour
 
     private int currentState;
 
-    private GameObject tutorialEnemy;
+    [Header("Unity Events")]
+    [SerializeField] UnityEvent PlayerLearnedMove;
+    [SerializeField] UnityEvent PlayerLearnedReflect;
+    [SerializeField] UnityEvent PlayerLearnedAttack;
+    [SerializeField] UnityEvent PlayerKilledEnemy;
+    [SerializeField] UnityEvent PlayerVialFull;
+    [SerializeField] UnityEvent PlayerHealed;
 
     void Awake()
     {
@@ -33,63 +40,135 @@ public class TutorialManager : MonoBehaviour
         currentState = 0;
     }
 
-    void Update()
+    public void PlayerMoves()
     {
-        if(EnemyManager.GetInstance().enemyCount == 0 && currentState == 3)
-        {
-            OnPlayerKilledEnemies();
-        }
-    }
-
-    public void PlayerMoved(InputAction.CallbackContext context)
-    {
-        if(currentState != 0)
-        {
-            return;
-        }
-
-        if (context.performed && !MissionLog.GetInstance().isUpdating)
+        if(currentState == 0 && !MissionLog.GetInstance().isUpdating)
         {
             currentState = 1;
-            StartCoroutine(MissionLog.GetInstance().UpdateLog("Press Space to Dash"));
+            PlayerLearnedMove.Invoke();
+            UpdateLog("Left click to reflect");
         }
     }
 
-    public void PlayerDashed(InputAction.CallbackContext context)
+    public void PlayerReflects()
     {
-        if(currentState != 1)
-        {
-            return;
-        }
-
-        if (context.performed && !MissionLog.GetInstance().isUpdating)
+        if (currentState == 1 && !MissionLog.GetInstance().isUpdating)
         {
             currentState = 2;
-            StartCoroutine(MissionLog.GetInstance().UpdateLog("Left Click to Reflect"));
+            PlayerLearnedReflect.Invoke();
+            UpdateLog("Reflect bullets back at enemies");
         }
     }
 
-    public void PlayerReflected(InputAction.CallbackContext context)
+    public void PlayerAttacks()
     {
-        if(currentState != 2)
-        {
-            return;
-        }
-
-        if (context.performed && !MissionLog.GetInstance().isUpdating)
+        if(currentState == 2 && EnemyManager.GetInstance().enemyCount == 0 && !MissionLog.GetInstance().isUpdating)
         {
             currentState = 3;
-            StartCoroutine(MissionLog.GetInstance().UpdateLog("Reflect bullets back at enemies"));
-            EnemyManager.GetInstance().SpawnEnemy(0, new Vector3(3, -10, 0));
+            PlayerLearnedAttack.Invoke();
+            UpdateLog("Kill more enemies to fill up the vial");
         }
     }
 
-    public void OnPlayerKilledEnemies()
+    public void PlayerFillsVial()
     {
-        currentState = 4;
-        PlayerManager.GetInstance().AddVialPoint(10);
-        StartCoroutine(MissionLog.GetInstance().UpdateLog("Click R to heal"));
+        if(currentState == 3 && PlayerManager.GetInstance().VialFullState() && !MissionLog.GetInstance().isUpdating)
+        {
+            currentState = 4;
+            PlayerVialFull.Invoke();
+            UpdateLog("Click R to heal");
+        }
     }
+
+    public void PlayerHeals()
+    {
+        if(currentState == 4 && !MissionLog.GetInstance().isUpdating)
+        {
+            currentState = 5;
+            PlayerHealed.Invoke();
+            UpdateLog("End of the tutorial");
+        }
+    }
+
+    public void UpdateLog(string message)
+    {
+        StartCoroutine(MissionLog.GetInstance().UpdateLog(message));
+    }
+
+    public void SpawnFirstEnemy()
+    {
+        EnemyManager.GetInstance().SpawnEnemy(0, new Vector3(3, -10, 0));
+    }
+
+    public void SpawnNewEnemy()
+    {
+        if(!PlayerManager.GetInstance().VialFullState())
+        {
+            EnemyManager.GetInstance().SpawnEnemy(0, new Vector3(3, -10, 0));
+        }
+        else
+        {
+            PlayerFillsVial();
+        }
+    }
+
+    // void Update()
+    // {
+    //     if(EnemyManager.GetInstance().enemyCount == 0 && currentState == 3)
+    //     {
+    //         OnPlayerKilledEnemies();
+    //     }
+    // }
+
+    // public void PlayerMoved(InputAction.CallbackContext context)
+    // {
+    //     if(currentState != 0)
+    //     {
+    //         return;
+    //     }
+
+    //     if (context.performed && !MissionLog.GetInstance().isUpdating)
+    //     {
+    //         currentState = 1;
+    //         StartCoroutine(MissionLog.GetInstance().UpdateLog("Press Space to Dash"));
+    //     }
+    // }
+
+    // public void PlayerDashed(InputAction.CallbackContext context)
+    // {
+    //     if(currentState != 1)
+    //     {
+    //         return;
+    //     }
+
+    //     if (context.performed && !MissionLog.GetInstance().isUpdating)
+    //     {
+    //         currentState = 2;
+    //         StartCoroutine(MissionLog.GetInstance().UpdateLog("Left Click to Reflect"));
+    //     }
+    // }
+
+    // public void PlayerReflected(InputAction.CallbackContext context)
+    // {
+    //     if(currentState != 2)
+    //     {
+    //         return;
+    //     }
+
+    //     if (context.performed && !MissionLog.GetInstance().isUpdating)
+    //     {
+    //         currentState = 3;
+    //         StartCoroutine(MissionLog.GetInstance().UpdateLog("Reflect bullets back at enemies"));
+    //         EnemyManager.GetInstance().SpawnEnemy(0, new Vector3(3, -10, 0));
+    //     }
+    // }
+
+    // public void OnPlayerKilledEnemies()
+    // {
+    //     currentState = 4;
+    //     PlayerManager.GetInstance().AddVialPoint(10);
+    //     StartCoroutine(MissionLog.GetInstance().UpdateLog("Click R to heal"));
+    // }
 }
 
 // 0: WASD to move

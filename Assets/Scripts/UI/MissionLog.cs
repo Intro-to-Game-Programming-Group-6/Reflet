d92215943559e381ee0bf2759f259fe988a95ce5
unknown <a.ariyanuchitkul.com>
2023-12-18 01:54:17 +0800

    Mission log revision

--- Assets/Scripts/UI/MissionLog.cs
+++ Assets/Scripts/UI/MissionLog.cs
@@ -12,8 +12,8 @@ public class MissionLog : MonoBehaviour
     public float duration;
     public bool isUpdating;
 
-    private Vector3 basePosition = new Vector3(290, 280, 0);
-    private Vector3 secondaryPosition = new Vector3(290, 400, 0);
+    private Vector3 basePosition = new Vector3(0, 0, 0); //new Vector3(290, 280, 0);
+    private Vector3 secondaryPosition = new Vector3(0, 0, 0);  //Vector3(290, 400, 0);
 
     void Awake()
     {
@@ -35,6 +35,8 @@ public class MissionLog : MonoBehaviour
     void Start()
     {
         isUpdating = false;
+        basePosition = log.transform.position + new Vector3(0, -80, 0);
+        secondaryPosition = log.transform.position + new Vector3(0, 30, 0);
     }
 
     public IEnumerator UpdateLog(string value)

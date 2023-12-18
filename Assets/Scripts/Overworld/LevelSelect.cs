f43f463f0023cf1933a0e487bf44ab163d253951
kevin <k.richardsonhalim2002@gmail.com>
2023-12-18 18:13:55 +0800

    UpgradeSelectPanel Prefab (callback unimplemented yet)

--- Assets/Scripts/Overworld/LevelSelect.cs
+++ Assets/Scripts/Overworld/LevelSelect.cs
@@ -10,74 +10,131 @@ public class LevelSelect : MonoBehaviour
     int m_upperLimit;
     int m_lowerLimit;
     [SerializeField] int m_size;
-    [SerializeField] int m_levelId;
-    [SerializeField] private Button[] m_buttons;
-    [SerializeField] private Button m_startButton;
-    [SerializeField] private TextMeshProUGUI m_levelIdText;
-    
+    [SerializeField] int m_selectedId;
+
+    [SerializeField] private GameObject[] m_buttonObjects;
+    private Button[] m_buttonButtons;
+    private Image[] m_buttonImages;
+    private TextMeshProUGUI[] m_buttonTMPs;
+/*
+    [SerializeField] private GameObject m_okButtonObject;
+    private Button m_okButtonButton;
+    private TextMeshProUGUI m_okButtonTMP;
+ */ 
     private InputAction m_upArrowAction;
     private InputAction m_downArrowAction;
     private InputAction m_enterAction;
 
     void Awake()
     {
-        m_upArrowAction = new InputAction(binding: "<Keyboard>/upArrow");
-        m_downArrowAction = new InputAction(binding: "<Keyboard>/downArrow");
-        m_enterAction = new InputAction(binding: "<Keyboard>/enter");
+        if (m_buttonButtons.Length == 0)
+        {
+            Debug.LogError("No buttons in the `m_buttonObjects` array");
+        }
 
-        m_upArrowAction.performed += _ => SelectLevel(Mathf.Min(m_levelId + 1, m_upperLimit));
-        m_downArrowAction.performed += _ => SelectLevel(Mathf.Max(m_levelId - 1, m_lowerLimit));
-        m_enterAction.performed += _ => { if (m_startButton.interactable) LevelManager.GetInstance().LoadLevel(m_levelId); };
+        SetupSelectButtons();
+        m_selectedId = -1;
 
-        m_startButton.onClick.AddListener(() => LevelManager.GetInstance().LoadLevel(m_levelId));
-        m_startButton.interactable = false;
-        
-        int i = 0;
-        foreach (Button btns in m_buttons)
+        // SetupOKButton();
+        // m_okButtonTMP.text = "Confirm";
+    }
+
+    void SetupButtonAt(int buttonIdx, Sprite buttonSprite, string buttonTitle = "", System.Action callback = null)
+    {
+        if (buttonIdx >= m_buttonButtons.Length)
+        {
+            Debug.LogWarning("Button id at [" + buttonIdx + "] is out of range. Buttons length: " + m_buttonButtons.Length + ".");
+            return;
+        }
+
+        // TODO: implement callback
+        // m_buttonButtons[buttonIdx].onClick.AddListener(() => ... callback);
+        m_buttonImages[buttonIdx].sprite = buttonSprite;
+        m_buttonTMPs[buttonIdx].text = buttonTitle;
+    }
+
+    void SetupSelectButtons()
+    {
+        int i;
+
+        // Get Components
+        for (i = 0; i < m_buttonObjects.Length; i++)
+        {
+            m_buttonButtons[i] = m_buttonObjects[i].GetComponent<Button>();
+            m_buttonImages[i] = m_buttonObjects[i].GetComponent<Image>();
+            m_buttonTMPs[i] = m_buttonObjects[i].GetComponent<TextMeshProUGUI>();
+        }
+
+        // Setup Interactabilty
+        m_upArrowAction = new InputAction(binding: "<Keyboard>/rightArrow");
+        m_downArrowAction = new InputAction(binding: "<Keyboard>/leftArrow");
+
+        m_upArrowAction.performed += _ => SelectLevel(Mathf.Min(m_selectedId + 1, m_upperLimit));
+        m_downArrowAction.performed += _ => SelectLevel(Mathf.Max(m_selectedId - 1, m_lowerLimit));
+
+        // Setup Functionality
+        for (i = 0; i < m_buttonButtons.Length; i++)
         {
             int x = i;
-            if (i < m_size) {
-                btns.onClick.AddListener(() => SelectLevel(x));
-                i++;
+            if (i < m_size)
+            {
+                m_buttonButtons[i].onClick.AddListener(() => SelectLevel(x));
             }
-            else {
-                btns.interactable = false;
+            else
+            {
+                m_buttonButtons[i].interactable = false;
             }
         }
-
         m_upperLimit = (m_size < i) ? m_size - 1 : i - 1;
-        m_lowerLimit = 0; 
-
-        m_levelId = -1;
-        m_levelIdText.text = "-";
-
-        if (m_buttons.Length == 0)
-        {
-            Debug.LogError("No buttons in the m_buttons array");
-        }
+        m_lowerLimit = 0;
     }
+/*
+    void SetupOKButton()
+    {
+        if (m_okButtonObject == null) return;
+
+        m_okButtonButton = m_okButtonObject.GetComponent<Button>();
+        m_okButtonTMP = m_okButtonObject.GetComponent<TextMeshProUGUI>();
+
+        m_enterAction = new InputAction(binding: "<Keyboard>/enter");
+        m_enterAction.performed += _ => { if (m_okButtonButton.interactable) LevelManager.GetInstance().LoadLevel(m_selectedId); };
+
+        m_okButtonButton.onClick.AddListener(() => LevelManager.GetInstance().LoadLevel(m_selectedId));
+        m_okButtonButton.interactable = false;
+    }
+*/
 
     void OnEnable()
     {
         m_upArrowAction.Enable();
         m_downArrowAction.Enable();
-        m_enterAction.Enable();
+/*
+        if (m_okButtonObject == null)
+            m_enterAction.Enable();
+*/
     }
 
     void OnDisable()
     {
         m_upArrowAction.Disable();
         m_downArrowAction.Disable();
-        m_enterAction.Disable();
-    }
+/*
+        if (m_okButtonObject == null)
+            m_enterAction.Disable();
+*/
+     }
         
     public void SelectLevel(int level) {
         Debug.Log("Selecting level " + level);
-        if (m_levelId >= 0) m_buttons[m_levelId].OnPointerExit(null);            
-        m_levelId = level;
-        m_buttons[m_levelId].Select();
-        LevelManager.GetInstance().SetLevel(m_levelId);
-        m_levelIdText.text = "" + (m_levelId + 1);
-        m_startButton.interactable = true;
+        if (m_selectedId >= 0)  m_buttonButtons[m_selectedId].OnPointerExit(null);            
+        m_selectedId = level;
+         m_buttonButtons[m_selectedId].Select();
+        LevelManager.GetInstance().SetLevel(m_selectedId);
+
+        // TODO: Change Scene since its been selected by... OR implement at the callback directly.
+/*
+        m_okButtonTMP.text = "" + (m_selectedId + 1);
+        m_okButtonButton.interactable = true;
+*/
     }
 }
